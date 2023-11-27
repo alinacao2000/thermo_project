4cac5aa9e1b12a700d670af0cb8a571cc8ba884c
alinacao2000 <yicao.alina@gmail.com>
2023-11-27 17:27:02 -0500

    Pass the test, ready to sumbit; but the opp is not precisely defined, making the crystal disordered

--- main.py
+++ main.py
@@ -1,45 +1,116 @@
+### Initialize ###
+
+# Import modules
+import datetime
 import hoomd
-import hoomd.md
-import math
 import numpy as np
+import math
+import gsd.hoomd
+import itertools
+
+# Define simulation device
+cpu = hoomd.device.CPU()
+
+# Create simulation object
+sim = hoomd.Simulation(device=cpu, seed=777)
+
+################
+
+### Define initial condition ###
 
 # Parameters
-particleNumber = 4096
+particleNumber = 409
 numberDensity = 0.03
 temperature = 0.25
-potential_k = 8.00
-potential_phi = 0.53
-timeSteps = int(50e6)
+timeSteps = int(10e6)
 
-# Calculate the box size
+# create a initial box size
+box_size = math.pow(particleNumber/numberDensity, 1.0 / 3.0)
+
+# Calculate the box size (different from the homework script)
 volume = particleNumber / numberDensity
-box_size = volume ** (1. / 3.)
 
-# Initialize a system with particles placed at random
-snapshot = hoomd.Snapshot()
-snapshot.particles.N = particleNumber
-snapshot.particles.types = ['A']
+x = np.linspace(-box_size / 2, box_size / 2, 10, endpoint=False)
+position = np.array(list(itertools.product(x, repeat=3)))
+position = position[:particleNumber]
+
+# Define snapshot
+snapshot = gsd.hoomd.Snapshot()
+snapshot.particles.N = len(position)
+snapshot.particles.position = position
+snapshot.particles.typeid = [0]*len(position)
 snapshot.configuration.box = [box_size, box_size, box_size, 0, 0, 0]
-snapshot.particles.position[:] = np.random.uniform(-box_size / 2., box_size / 2., size=(particleNumber, 3))
+snapshot.particles.types = ['A']
 
-# Initialize the simulation
-sim = hoomd.Simulation(device=hoomd.device.CPU(), seed=1)
+# Create snapshot
 sim.create_state_from_snapshot(snapshot)
 
-# Pair potential table
-nl = hoomd.md.nlist.Cell(0.01)
-table = hoomd.md.pair.Table(nlist=nl)
-table.params[('A', 'A')] = dict(func=OPP, rmin=0.5, rmax=2.5, coeff=dict(k=potential_k, phi=potential_phi))
+################################
 
-# Integrate at constant temperature
-nvt = hoomd.md.methods.NVT(kT=temperature, filter=hoomd.filter.All(), tau=1.0)
-integrate = hoomd.md.Integrator(dt=0.005, methods=[nvt])
-sim.operations.integrator = integrate
+## Define integration ###
 
-# Log the simulation data
-logger = hoomd.logging.Logger()
-logger.add(sim, quantities=['potential_energy', 'temperature'], list=True)
-sim.operations.writers.append(hoomd.write.Table(trigger=hoomd.trigger.Periodic(period=100), logger=logger, mode='txt', file='log-output.log'))
+# Parameters
+kbt = 0.25
+dt = 1.0E-3
 
-# Run the simulation
-sim.run(timeSteps)
+# Set up integrator
+integrator = hoomd.md.Integrator(dt=dt)
+
+# Cell list
+cell = hoomd.md.nlist.Cell(buffer=0.4)
+
+potential_k = 8.00
+potential_phi = 0.53
+opp = hoomd.md.pair.OPP(cell)  # should we change the default_r_cut?
+opp.params[('A', 'A')] = {
+    'C1': 1., 'C2': 1., 'eta1': 15,
+    'eta2': 3, 'k': potential_k, 'phi': potential_phi}
+opp.r_cut[('A', 'A')] = 3.0
+integrator.forces.append(opp)
+
+# Define ensemble and add to integrator
+full = hoomd.filter.All()
+nvt = hoomd.md.methods.NVT(kT=kbt, filter=full, tau=1.0)
+integrator.methods.append(nvt)
+
+# Assign to simulation
+sim.operations.integrator = integrator
+
+# Thermalize
+sim.state.thermalize_particle_momenta(filter=full, kT=kbt)
+
+### Define logger for tps printing ###
+logger = hoomd.logging.Logger(categories=['scalar', 'string'])
+logger.add(sim, quantities=['timestep', 'tps'])
+class Status():
+    def __init__(self, sim):
+        self.sim = sim
+    @property
+    def seconds_remaining(self):
+        try:
+            return (self.sim.final_timestep - self.sim.timestep) / self.sim.tps
+        except ZeroDivisionError:
+            return 0
+    @property
+    def etr(self):
+        return str(datetime.timedelta(seconds=self.seconds_remaining))
+status = Status(sim)
+logger[('Status', 'etr')] = (status, 'etr', 'string')
+table = hoomd.write.Table(trigger=hoomd.trigger.Periodic(period=5000),
+                          logger=logger)
+sim.operations.writers.append(table)
+######################################
+
+
+# Write to file
+ndump = 1000
+gsd_writer = hoomd.write.GSD(filename='sample.gsd',
+                             trigger=hoomd.trigger.Periodic(ndump),
+                             mode='wb')
+sim.operations.writers.append(gsd_writer)
+
+# Run
+sim.run(1E6)
+
+#########################
+
